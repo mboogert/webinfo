c3bbd9477ac1f603ff262498dfb6c7a0bea7b450
Marcel Boogert <mboogert@proserve.nl>
2017-10-21 21:43:55 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,13 @@ FROM php:7-apache
 
 MAINTAINER Marcel Boogert <marcel@mtdb.nl>
 
+RUN apt-get update && \
+    apt-get -y install git && \
+    apt-get clean
+
+RUN cd /var/www/html && \
+    git clone git://github.com/nrk/predis.git
+
 ADD background.png /var/www/html/background.png
 ADD background-left.png /var/www/html/background-left.png
 ADD background-right.png /var/www/html/background-right.png
@@ -9,6 +16,10 @@ ADD background-right.png /var/www/html/background-right.png
 ADD swarm-moby-blue.png /var/www/html/swarm-moby-blue.png
 ADD swarm-moby-green.png /var/www/html/swarm-moby-green.png
 
+ADD docker-host-blue.png /var/www/html/docker-host-blue.png
+ADD docker-host-green.png /var/www/html/docker-host-green.png
+
 ADD index.php /var/www/html/index.php
+ADD redis.php /var/www/html/redis.php
 
 EXPOSE 80
--- index.php
+++ index.php
@@ -7,6 +7,16 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 ?>
 
 <?php
+  require "predis/autoload.php";
+  Predis\Autoloader::register();
+
+  $redis = new Predis\Client(array(
+    "scheme" => "tcp",
+    "host" => "swarmdemo_redis",
+    "port" => 6379));
+?>
+
+<?php
 $file_content = file('/etc/hostname', FILE_IGNORE_NEW_LINES);
 $container_hostname = $file_content[0];
 ?>
@@ -15,6 +25,53 @@ $container_hostname = $file_content[0];
 $dc1 = array("pvm11150.proservers.nl", "pvm11151.proservers.nl", "pvm11152.proservers.nl");
 $dc2 = array("pvm11030.proserve.nl", "pvm11031.proserve.nl", "pvm11032.proserve.nl");
 $local = array("localhost", "lt30");
+
+$pvm11150_color = "blue";
+$pvm11151_color = "blue";
+$pvm11152_color = "blue";
+$pvm11030_color = "blue";
+$pvm11031_color = "blue";
+$pvm11032_color = "blue";
+
+$pvm11150_counter = ($redis -> get('pvm11150_counter'));
+$pvm11151_counter = ($redis -> get('pvm11151_counter'));
+$pvm11152_counter = ($redis -> get('pvm11152_counter'));
+$pvm11030_counter = ($redis -> get('pvm11030_counter'));
+$pvm11031_counter = ($redis -> get('pvm11031_counter'));
+$pvm11032_counter = ($redis -> get('pvm11032_counter'));
+
+switch ($container_hostname) {
+    case "pvm11150.proservers.nl":
+	$pvm11150_color = "green";
+	$pvm11150_counter++;
+        $redis -> set('pvm11150_counter', $pvm11150_counter);
+        break;
+    case "pvm11151.proservers.nl":
+        $pvm11151_color = "green";
+	$pvm11151_counter++;
+        $redis -> set('pvm11151_counter', $pvm11151_counter);
+        break;
+    case "pvm11152.proservers.nl":
+        $pvm11152_color = "green";
+	$pvm11152_counter++;
+        $redis -> set('pvm11152_counter', $pvm11152_counter);
+        break;
+    case "pvm11030.proserve.nl":
+        $pvm11030_color = "green";
+	$pvm11030_counter++;
+        $redis -> set('pvm11030_counter', $pvm11030_counter);
+        break;
+    case "pvm11031.proserve.nl":
+        $pvm11031_color = "green";
+	$pvm11031_counter++;
+        $redis -> set('pvm11031_counter', $pvm11031_counter);
+        break;
+    case "pvm11032.proserve.nl":
+        $pvm11032_color = "green";
+	$pvm11032_counter++;
+        $redis -> set('pvm11032_counter', $pvm11032_counter);
+        break;
+}
 
 if (in_array($container_hostname, $dc1)) {
         $datacenter = "dc1";
@@ -38,7 +95,7 @@ if (in_array($container_hostname, $local)) {
 
 <html>
 <head>
-        <title>Hi, my name is...</title>
+        <title>Docker Swarm HA</title>
         <meta http-equiv="refresh" content="1">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
         <style>
@@ -55,44 +112,26 @@ if (in_array($container_hostname, $local)) {
         </style>
 </head>
 <body>
-<<<<<<< HEAD
-	<div style="background-image: url(/background.png); position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; height: 820px; width: 703px;">
+<div style="background-image: url(/background.png); position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; height: 820px; width: 703px;">
 	<div style="background-image: url(/background-<?php print $background_direction; ?>.png); position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; height: 820px; width: 703px;">
-		<div style="background-image: url(/swarm-moby-<?php print $dc1_color; ?>.png); position: absolute; left: 50; top: 600; width: 200px; height: 108;"></div>
-		<div style="background-image: url(/swarm-moby-<?php print $dc2_color; ?>.png); position: absolute; right: 50; top: 600; width: 200px; height: 108px;"></div>
-		<div style="position: absolute; left: 50; top: 450; width: 250px; height: 200px;"><?php if($background_direction=="left"){echo "worker: " . $container_hostname;}?></div>
-		<div style="position: absolute; right: 50; top: 450; width: 250px; height: 200px;"><?php if($background_direction=="right"){echo "worker: " . $container_hostname;}?></div>
-		<div style="position: absolute; left: 50; top: 500; width: 250px; height: 200px;"><?php if($background_direction=="left"){echo "ip: " . $_SERVER["SERVER_ADDR"];}?></div>
-		<div style="position: absolute; right: 50; top: 500; width: 250px; height: 200px;"><?php if($background_direction=="right"){echo "ip: " . $_SERVER["SERVER_ADDR"];}?></div>
-		<div style="position: absolute; left: 50; top: 550; width: 250px; height: 200px;"><?php if($background_direction=="left"){echo "container: " . $_ENV["HOSTNAME"];}?></div>
-		<div style="position: absolute; right: 50; top: 550; width: 250px; height: 200px;"><?php if($background_direction=="right"){echo "container: " . $_ENV["HOSTNAME"];}?></div>
+		<div style="background-image: url(/swarm-moby-<?php print $dc1_color; ?>.png); position: absolute; left: 75; top: 450; width: 200px; height: 108;"></div>
+		<div style="background-image: url(/swarm-moby-<?php print $dc2_color; ?>.png); position: absolute; right: 75; top: 450; width: 200px; height: 108px;"></div>
+
+		<div style="background-image: url(/docker-host-<?php print $pvm11150_color; ?>.png); position: absolute; left: 25; top: 600; width: 84px; height: 111px;"></div>
+		<div style="position: absolute; left: 25; top: 710; width: 55; text-align: center;"><b><?php print $pvm11150_counter; ?></b></div>
+		<div style="background-image: url(/docker-host-<?php print $pvm11151_color; ?>.png); position: absolute; left: 140; top: 600; width: 84px; height: 111px;"></div>
+		<div style="position: absolute; left: 140; top: 710; width: 55; text-align: center;"><b><?php print $pvm11151_counter; ?></b></div>
+		<div style="background-image: url(/docker-host-<?php print $pvm11152_color; ?>.png); position: absolute; left: 250; top: 600; width: 84px; height: 111px;"></div>
+		<div style="position: absolute; left: 250; top: 710; width: 55; text-align: center;"><b><?php print $pvm11152_counter; ?></b></div>
+
+		<div style="background-image: url(/docker-host-<?php print $pvm11030_color; ?>.png); position: absolute; right: 25; top: 600; width: 84px; height: 111px;"></div>
+		<div style="position: absolute; right: 25; top: 710; width: 55; text-align: center;"><b><?php print $pvm11030_counter; ?></b></div>
+		<div style="background-image: url(/docker-host-<?php print $pvm11031_color; ?>.png); position: absolute; right: 140; top: 600; width: 84px; height: 111px;"></div>
+		<div style="position: absolute; right: 140; top: 710; width: 55; text-align: center;"><b><?php print $pvm11031_counter; ?></b></div>
+		<div style="background-image: url(/docker-host-<?php print $pvm11032_color; ?>.png); position: absolute; right: 250; top: 600; width: 84px; height: 111px;"></div>
+		<div style="position: absolute; right: 250; top: 710; width: 55; text-align: center;"><b><?php print $pvm11032_counter; ?></b></div>
+
 	</div>
-=======
-        <div style="background-image: url(/background.png); position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; height: 820px; width: 703px;">
-        <div style="background-image: url(/background-<?php print $background_direction; ?>.png); position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; height: 820px; width: 703px;">
-                <div style="background-image: url(/swarm-moby-<?php print $dc1_color; ?>.png); position: absolute; left: 50; top: 600; width: 200px; height: 108;"></div>
-                <div style="background-image: url(/swarm-moby-<?php print $dc2_color; ?>.png); position: absolute; right: 50; top: 600; width: 200px; height: 108px;"></div>
-                <div style="position: absolute; left: 50; top: 450; width: 250px; height: 200px;"><?php if($background_direction=="left"){echo "swarm worker : " . $container_hostname;}?></div>
-                <div style="position: absolute; right: 50; top: 450; width: 250px; height: 200px;"><?php if($background_direction=="right"){echo "swarm worker : " . $container_hostname;}?></div>
-                <div style="position: absolute; left: 50; top: 500; width: 250px; height: 200px;"><?php if($background_direction=="left"){echo "ip address" . $_SERVER["SERVER_ADDR"];}?></div>
-                <div style="position: absolute; right: 50; top: 500; width: 250px; height: 200px;"><?php if($background_direction=="right"){echo "ip address" . $_SERVER["SERVER_ADDR"];}?></div>
-                <div style="position: absolute; left: 50; top: 550; width: 250px; height: 200px;"><?php if($background_direction=="left"){echo "container : " . $_ENV["HOSTNAME"];}?></div>
-                <div style="position: absolute; right: 50; top: 550; width: 250px; height: 200px;"><?php if($background_direction=="right"){echo "container : " . $_ENV["HOSTNAME"];}?></div>
-        </div>
->>>>>>> 06c9300d2297d85a56cc2b5d677a66217c0012cb
-        <?php
-        $links = [];
-        foreach($_ENV as $key => $value) {
-                if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-                        $links[] = [
-                                "name" => $matches[1],
-                                "port" => $matches[2],
-                                "proto" => $matches[3],
-                                "value" => $value
-                        ];
-                }
-        }
-?>
 </div>
 </body>
 </html>
